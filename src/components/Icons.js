957a57eebe8283dd3c90866e40595897f02c2f80
equidistant <mich.random@pm.me>
2020-06-25 14:39:31 +0200

    cover page responsiveness

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -41,3 +41,58 @@ export const HamburgerIcon = () => {
     </HamburgerSvg>
   )
 }
+
+const ArrowDownSvg = styled.svg`
+  width: 79px;
+  height: 34px;
+  width: 60px;
+  height: 37px;
+  @media only screen and (max-width: 1280px) {
+    width: calc(79px / 1.4);
+    height: calc(34px / 1.4);
+  }
+  @media only screen and (max-width: 720px) {
+    width: calc(79px / 1.8);
+    height: calc(34px / 1.8);
+  }
+  @media only screen and (max-width: 400px), only screen and (max-height: 500px) {
+    width: calc(79px / 2.2);
+    height: calc(34px / 2.2);
+  }
+`
+
+export const ArrowDownIcon = memo(() => {
+  return (
+    <ArrowDownSvg viewBox='0 0 79 34' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path fill='#FFFBF6' fillRule='evenodd' clipRule='evenodd' d='M77.308 3.77456C78.3854 2.91107 78.3854 1.51109 77.308 0.647611C76.2307 -0.21587 74.4839 -0.21587 73.4065 0.647611L39.058 28.1765L4.70961 0.647611C3.63222 -0.21587 1.88543 -0.21587 0.808041 0.647611C-0.269347 1.51109 -0.269347 2.91107 0.808041 3.77456L36.3203 32.2361C36.4547 32.5142 36.662 32.7749 36.942 32.9993C38.0194 33.8628 39.7662 33.8628 40.8436 32.9993L77.308 3.77456Z'/>
+    </ArrowDownSvg>
+  )
+})
+
+const FacebookLightSvg = styled.svg`
+  width: 40px;
+  height: 40px;
+`
+
+export const FacebookLightIcon = memo(() => {
+  return (
+    <FacebookLightSvg viewBox='0 0 40 40' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M30.0001 3.33334H25.0001C22.7899 3.33334 20.6703 4.21132 19.1075 5.77412C17.5447 7.33692 16.6667 9.45654 16.6667 11.6667V16.6667H11.6667V23.3333H16.6667V36.6667H23.3334V23.3333H28.3334L30.0001 16.6667H23.3334V11.6667C23.3334 11.2246 23.509 10.8007 23.8216 10.4882C24.1341 10.1756 24.5581 10 25.0001 10H30.0001V3.33334Z' stroke='#FFFBF7' strokeWidth='3' strokeLinecap='round' strokeLinejoin='round'/>
+    </FacebookLightSvg>
+  )
+})
+
+const InstagramLightSvg = styled.svg`
+  width: 40px;
+  height: 40px;
+`
+
+export const InstagramLightIcon = memo(() => {
+  return (
+    <InstagramLightSvg viewBox='0 0 40 40' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M28.3333 3.33334H11.6666C7.06421 3.33334 3.33325 7.0643 3.33325 11.6667V28.3333C3.33325 32.9357 7.06421 36.6667 11.6666 36.6667H28.3333C32.9356 36.6667 36.6666 32.9357 36.6666 28.3333V11.6667C36.6666 7.0643 32.9356 3.33334 28.3333 3.33334Z' stroke='#FFFBF7' strokeWidth='3' strokeLinecap='round' strokeLinejoin='round'/>
+      <path d='M26.6667 18.95C26.8724 20.3371 26.6355 21.7537 25.9897 22.9983C25.3438 24.243 24.322 25.2523 23.0695 25.8828C21.8169 26.5132 20.3975 26.7326 19.0131 26.5099C17.6286 26.2871 16.3497 25.6335 15.3581 24.6419C14.3666 23.6504 13.713 22.3714 13.4902 20.987C13.2674 19.6026 13.4869 18.1831 14.1173 16.9306C14.7477 15.6781 15.7571 14.6562 17.0017 14.0104C18.2464 13.3646 19.663 13.1276 21.0501 13.3333C22.465 13.5431 23.7748 14.2024 24.7862 15.2138C25.7976 16.2252 26.4569 17.5351 26.6667 18.95Z' stroke='#FFFBF7' strokeWidth='3' strokeLinecap='round' strokeLinejoin='round'/>
+      <path d='M29.1667 10.8333H29.1834' stroke='#FFFBF7' strokeWidth='4' strokeLinecap='round' strokeLinejoin='round'/>
+    </InstagramLightSvg>
+  )
+})
--- src/modules/home/Cover.js
+++ src/modules/home/Cover.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { cover, eagle2, photo } from '../../images'
-import { HamburgerButton } from '../../components'
+import { HamburgerButton, ArrowDownIcon, FacebookLightIcon, InstagramLightIcon } from '../../components'
 
 const Cover = () => {
   return (
@@ -20,10 +20,24 @@ const Cover = () => {
           <H1>photography</H1>
         </TextRow>
         <TextRow>
-          <H2 style={{ 'margin-right': '10px' }}>The world</H2>
+          <H2 style={{ 'marginRight': '10px' }}>The world</H2>
           <H2>through my lens</H2>
         </TextRow>
+        <OutlinedButton>
+          View my work
+        </OutlinedButton>
       </MiddleContainer>
+      <ReadMoreContainer>
+        <H3>Read more about me</H3>
+        <ArrowDownIcon />
+      </ReadMoreContainer>
+      <BottomContainer>
+        <SocialIcons>
+          <FacebookLightIcon />
+          <InstagramLightIcon />
+        </SocialIcons>
+        <ContactMail>little.eagle.photo@pm.me</ContactMail>
+      </BottomContainer>
     </RootContainer>
   )
 }
@@ -48,8 +62,8 @@ const TopContainer = styled.div`
   padding-right: 40px;
   padding-top: 40px;
   @media only screen and (max-width: 1024px) {
-    padding-right: calc(40px / 1.6);
-    padding-top: calc(40px / 1.6);
+    padding-right: calc(40px / 1.4);
+    padding-top: calc(40px / 1.4);
   }
 `
 
@@ -63,9 +77,13 @@ const MiddleContainer = styled.div`
   top: 20%;
   padding-left: 100px;
   padding-right: 100px;
-  @media only screen and (max-width: 1024px) {
+  @media only screen and (max-width: 1280px) {
+    padding-left: calc(100px / 1.4);
+    padding-right: calc(100px / 1.4);
+  }
+  @media only screen and (max-width: 720px) {
     top: 15%;
-    padding-left: calc(100px / 2.5);
+    padding-left: calc(100px / 1.8);
     padding-right: calc(100px / 1.8);
   }
   @media only screen and (max-width: 400px), only screen and (max-height: 500px) {
@@ -78,8 +96,11 @@ const MiddleContainer = styled.div`
 const EaglePhotoContainer = styled.div`
   position: relative;
   height: 125px;
-  @media only screen and (max-width: 1024px) {
-    height: calc(125px / 1.6);
+  @media only screen and (max-width: 1280px) {
+    height: calc(125px / 1.4);
+  }
+  @media only screen and (max-width: 720px) {
+    height: calc(125px / 1.8);
   }
   @media only screen and (max-width: 400px), only screen and (max-height: 500px) {
     height: calc(125px / 2.2);
@@ -92,11 +113,17 @@ const Eagle2Img = styled.img`
   position: absolute;
   top: -100px;
   left: 175px;
-  @media only screen and (max-width: 1024px) {
-    width: calc(165px / 1.6);
-    height: calc(145px / 1.6);
-    top: calc(-100px / 1.6);
-    left: calc(175px / 1.6);
+  @media only screen and (max-width: 1280px) {
+    width: calc(165px / 1.4);
+    height: calc(145px / 1.4);
+    top: calc(-100px / 1.4);
+    left: calc(175px / 1.4);
+  }
+  @media only screen and (max-width: 720px) {
+    width: calc(165px / 1.8);
+    height: calc(145px / 1.8);
+    top: calc(-100px / 1.8);
+    left: calc(175px / 1.8);
   }
   @media only screen and (max-width: 400px), only screen and (max-height: 500px) {
     width: calc(165px / 2.4);
@@ -110,16 +137,20 @@ const PhotoImg = styled.img`
   width: 300px;
   height: 200px;
   left: -25px;
-  @media only screen and (max-width: 1024px) {
-    width: calc(300px / 1.6);
-    height: calc(200px / 1.6);
-    left: calc(-25px / 1.6);
+  @media only screen and (max-width: 1280px) {
+    width: calc(300px / 1.4);
+    height: calc(200px / 1.4);
+    left: calc(-25px / 1.4);
+  }
+  @media only screen and (max-width: 720px) {
+    width: calc(300px / 1.8);
+    height: calc(200px / 1.8);
+    left: calc(-25px / 1.8);
   }
   @media only screen and (max-width: 400px), only screen and (max-height: 500px) {
     width: calc(300px / 2.2);
     height: calc(200px / 2.2);
     left: calc(-25px / 2.2);
-
   }
   position: absolute;
 `
@@ -141,9 +172,13 @@ const H1 = styled.div`
   color: #FFFBF7;
   text-shadow: 1.14737px 2.86842px 2.86842px rgba(0, 0, 0, 0.2);
   z-index: 2;
-  @media only screen and (max-width: 1024px) {
-    font-size: calc(72px / 1.6);
-    line-height: calc(97px / 1.6);
+  @media only screen and (max-width: 1280px) {
+    font-size: calc(72px / 1.4);
+    line-height: calc(97px / 1.4);
+  }
+  @media only screen and (max-width: 720px) {
+    font-size: calc(72px / 1.8);
+    line-height: calc(97px / 1.8);
   }
   @media only screen and (max-width: 400px), only screen and (max-height: 500px) {
     font-size: calc(72px / 2.2);
@@ -163,14 +198,146 @@ const H2 = styled.div`
   line-height: 82px;
   color: #FFD8C1;
   text-shadow: 2px 5px 5px rgba(0, 0, 0, 0.2);
-  @media only screen and (max-width: 1024px) {
-    font-size: calc(61px / 1.6);
-    line-height: calc(82px / 1.6);
+  @media only screen and (max-width: 1280px) {
+    font-size: calc(61px / 1.4);
+    line-height: calc(82px / 1.4);
+  }
+  @media only screen and (max-width: 720px) {
+    font-size: calc(61px / 1.8);
+    line-height: calc(82px / 1.8);
   }
   @media only screen and (max-width: 400px), only screen and (max-height: 500px) {
     font-size: calc(61px / 2.2);
     line-height: calc(82px / 2.2);
   }
+`
+
+const OutlinedButton = styled.button`
+  width: 394px;
+  height: 98px;
+  border: 5px solid #FFFBF7;
+  backdrop-filter: blur(4px);
+  font-family: Raleway;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 28px;
+  text-transform: uppercase;
+  color: #FFFBF7;
+  background-color: transparent;
+  margin-top: 20px;
+  cursor: pointer;
+  background-color: transparent;
+  &:active {
+    transform: translateY(2px);
+  }
+  @media only screen and (max-width: 1280px) {
+    border: 4px solid #FFFBF7;
+    width: calc(394px / 1.4);
+    height: calc(98px / 1.4);
+    font-size: calc(24px / 1.4);
+    line-height: calc(28px / 1.4);
+    margin-top: calc(20px / 1.4);
+  }
+  @media only screen and (max-width: 720px) {
+    border: 4px solid #FFFBF7;
+    width: calc(394px / 1.8);
+    height: calc(98px / 1.8);
+    font-size: calc(24px / 1.8);
+    line-height: calc(28px / 1.8);
+    margin-top: calc(20px / 1.8);
+  }
+  @media only screen and (max-width: 400px), only screen and (max-height: 500px) {
+    border: 3px solid #FFFBF7;
+    width: calc(394px / 2.2);
+    height: calc(98px / 2.2);
+    font-size: calc(24px / 2.2);
+    line-height: calc(28px / 2.2);
+    margin-top: calc(20px / 2.2);
+  }
+`
+
+const ReadMoreContainer = styled.div`
+  width: 394px;
+  height: 100px;
+  margin-left: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  position: absolute;
+  top: 80%;
+  @media only screen and (max-width: 1280px) {
+    width: calc(394px / 1.4);
+    height: calc(100px / 1.4);
+    margin-left: calc(100px / 1.4);
+  }
+  @media only screen and (max-width: 720px) {
+    width: calc(394px / 1.8);
+    height: calc(100px / 1.8);
+    margin-left: calc(100px / 1.8);
+  }
+  @media only screen and (max-width: 400px), only screen and (max-height: 500px) {
+    width: calc(394px / 2.2);
+    height: calc(100px / 2.2);
+    margin-left: calc(100px / 2.2);
+  }
+`
+
+const H3 = styled.div`
+  width: 100%;
+  font-family: Raleway;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 28px;
+  text-transform: uppercase;
+  color: #FFFBF7;
+  text-align: center;
+  @media only screen and (max-width: 1280px) {
+    font-size: calc(24px / 1.4);
+    line-height: calc(28px / 1.4);
+  }
+  @media only screen and (max-width: 720px) {
+    font-size: calc(24px / 1.8);
+    line-height: calc(28px / 1.8);
+  }
+  @media only screen and (max-width: 400px), only screen and (max-height: 500px) {
+    font-size: calc(24px / 2.2);
+    line-height: calc(28px / 2.2);
+  }
+`
+
+const BottomContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;;
+  bottom: 60px;
+  right: 40px;
+  @media only screen and (max-width: 1024px) {
+    display: none;
+  }
+`
+
+const SocialIcons = styled.div`
+  width: 100px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const ContactMail = styled.div`
+  font-family: Raleway;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 28px;
+  color: #FFFBF7;
+  margin-top: 20px;
 `
 
 export default Cover
