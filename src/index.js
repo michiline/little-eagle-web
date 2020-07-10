4e8d4f0e223026ec7dfa167ff679dc85a1c8b778
equidistant <mich.random@pm.me>
2020-07-10 09:33:01 +0200

    react snap removed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { hydrate, render } from 'react-dom'
+import ReactDOM from 'react-dom'
 import App from './App'
 
-const rootElement = document.getElementById('root')
-
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement)
-} else {
-  render(<App />, rootElement)
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
